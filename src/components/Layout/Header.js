fcb939a7ab158f66051100198e1f595a0c0843bf
Bryan Tacuri <tacuri_bryan@hotmail.com>
2023-01-04 23:53:14 -0500

    adding a Header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Tacuri Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="a table full of delicious food! "/>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
